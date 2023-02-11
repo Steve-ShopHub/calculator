637466c75d5db6ba5785c2a940b4eab4f6b2e084
Steve <steve@tryshophub.com>
2023-02-11 01:30:02 +0000

    Complete main calculator function

--- temp.js
+++ temp.js
@@ -11,7 +11,7 @@
 
 // Start: (display 0)
 // 5 (display 5)
-// + (save: A = 5, operator = +) (if B = null, return A[5]?)
+// + (save: A = display [5], operator = +) (if B = null, return A[5]?)
 // 1 (display = 1)
 // 5 (display = 15)
 // * (save B = display [15], A = operate(A, operator, B) 
@@ -34,16 +34,26 @@ for (const button of numberButtons) {
 };
 
 function numberPress(){
+    operatorEnable();
     console.log(this.value);
+    console.log(operator);
     if ((displayText.charAt(0) == '0')) {
         displayText = displayText.slice(1);
         displayText += this.value;
         console.log('Display text: ' + displayText);
         display.textContent = displayText;        
     }
-// Need to remove old display upon number after operator
-    if (b == 0){
+    else if (b == 0){ // operator just pressed
         b = null;
+        displayText = '';
+        displayText += this.value;
+        console.log('Display text: ' + displayText);
+        display.textContent = displayText;
+    }
+    else if (operator == '='){
+        a = null;
+        b = 0;
+        operator = null;
         displayText = '';
         displayText += this.value;
         console.log('Display text: ' + displayText);
@@ -64,11 +74,49 @@ for (const button of operatorButtons) {
 };
 
 function operatorPress(){
+    operatorEnable();
+    let id = this.id;
+    console.log(id);
+    if (a == null && operator == null) {
+    document.querySelector('#' + id).disabled = true;
     a = displayText;
     b = 0;
     operator = this.value;
     console.log('A: ' + a);
     console.log('Operator: ' + operator);
+    }
+    else if (this.value == '='){
+        b = displayText;
+        a = operate(a, operator, b);
+        displayText = a.toString();
+        display.textContent = displayText;
+        a = null;
+        b = null;
+        operator = null;
+        document.querySelector('#' + id).disabled = true;
+        console.log('B: ' + b);
+        console.log('A: ' + a);
+        console.log('Operator: ' + operator);
+    }
+    else {
+        b = displayText;
+        a = operate(a, operator, b);
+        console.log(a);
+        displayText = a.toString();
+        display.textContent = displayText;
+        operator = this.value;
+        b = 0;
+        document.querySelector('#' + id).disabled = true;
+        console.log('B: ' + b);
+        console.log('A: ' + a);
+        console.log('Operator: ' + operator);
+    }
+};
+
+function operatorEnable(){
+    for (const button of operatorButtons) {
+        button.removeAttribute('disabled');
+    };
 }
 
 const display = document.querySelector('#display');
@@ -85,6 +133,7 @@ clearButton.addEventListener('click', clear);
 
 
 function clear(){
+    operatorEnable();
     displayText = '0';
     display.textContent = displayText;
     a = 0;
@@ -95,9 +144,17 @@ const deleteButton = document.querySelector('#delete');
 deleteButton.addEventListener('click', deleteChar);
 
 function deleteChar() {
+    operatorEnable();
     displayText = displayText.slice(0, -1);
     display.textContent = displayText;
+    console.log(displayText.length)
+    if (displayText.length == 0){
+        displayText += 0;
+        display.textContent = displayText;
 }
+};
+
+
 
 
 /////////// Calculation functions
